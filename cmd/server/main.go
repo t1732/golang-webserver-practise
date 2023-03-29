3cebf79c8bb5eb83311b71f1cdb18628e73d76f8
t1732 <oo1732oo@gmail.com>
2023-03-29 17:05:48 +0900

    feat: echoカスタムエラーハンドリング

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"net/http"
@@ -15,7 +16,12 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
 )
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
 
 var (
 	appEnv string
@@ -50,6 +56,8 @@ func main() {
 	e.Use(middleware.RequestID())
 	e.Use(middleware.Logger())
 
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
 	// Start server
 	fmt.Printf("running... %s mode", appEnv)
 	go func() {
@@ -66,5 +74,33 @@ func main() {
 	defer cancel()
 	if err := e.Shutdown(ctx); err != nil {
 		e.Logger.Fatal(err)
+	}
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	msg := "Internal Server Error"
+
+	var herr *echo.HTTPError
+	switch {
+	case errors.Is(err, gorm.ErrRecordNotFound):
+		code = http.StatusNotFound
+		msg = err.Error()
+		c.Logger().Info(err)
+	case errors.As(err, &herr):
+		code = herr.Code
+		msg = herr.Message.(string)
+	default:
+	}
+
+	if code >= 500 {
+		c.Logger().Error(err)
+	} else {
+		c.Logger().Info(err)
+	}
+
+	resp := ErrorResponse{Message: msg}
+	if err := c.JSON(code, resp); err != nil {
+		c.Logger().Error(err)
 	}
 }
